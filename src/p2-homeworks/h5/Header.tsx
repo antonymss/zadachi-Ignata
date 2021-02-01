1ff880b92d331b71236db0fb2b9a20f197aebf2b
antonymss <demeshkevichanton@gmail.com>
2021-02-01 13:17:54 +0300

    refact

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Routes";
+import s from "./Header.module.css"
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
+    const [activeMenu, setActiveMenu] = useState<boolean>(false);
+    const toggleMenu = () => {
+        setActiveMenu(!activeMenu)
+    }
 
+    return (
+        <div className={s.menuWrapper}>
+            <div className={s.burgerMenu}
+                 onClick={toggleMenu}>
+                &#9776;
+            </div>
+            <div className={`${s.containerMenuLinks} ${activeMenu && s.activeMenu}`}>
+                <NavLink to={PATH.PRE_JUNIOR}
+                         activeClassName={s.active} className={s.menuLinks}>
+                    Pre
+                    Junior
+                </NavLink>
+                <NavLink to={PATH.JUNIOR}
+                         activeClassName={s.active}
+                         className={s.menuLinks}>
+                    Junior
+                </NavLink>
+                <NavLink to={PATH.JUNIORPLUS}
+                         activeClassName={s.active}
+                         className={s.menuLinks}>
+                    Junior
+                    +
+                </NavLink>
+            </div>
         </div>
     );
 }
