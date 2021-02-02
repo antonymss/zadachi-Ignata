457be286eea893475a09982173a238ff06e23811
antonymss <demeshkevichanton@gmail.com>
2021-02-02 15:00:44 +0300

    refact

--- src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
+++ src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -14,10 +14,16 @@ const SuperSelect: React.FC<SuperSelectPropsType> = (
         ...restProps
     }
 ) => {
-    const mappedOptions: any[] = []; // map options with key
+
+    // const mappedOptions: any[] = []; // map options with key
+    const mappedOptions: any[] | undefined =  options ? options.map((o,i)=> <option key ={i}> {o}</option> ) :[]
+
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-        // onChange, onChangeOption
+
+           // onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.value)
+
     }
 
     return (
